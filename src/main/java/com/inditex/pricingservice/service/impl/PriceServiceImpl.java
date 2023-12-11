a6ba0c400d95eca9af7b58467f3f974b5b246d7d
Sergio <sergioaldu11@gmail.com>
2023-12-11 23:49:54 +0100

    Add exception handling using @ControllerAdvice and @ExceptionHandler to improve error handling structure.

--- src/main/java/com/inditex/pricingservice/service/impl/PriceServiceImpl.java
+++ src/main/java/com/inditex/pricingservice/service/impl/PriceServiceImpl.java
@@ -2,15 +2,13 @@ package com.inditex.pricingservice.service.impl;
 
 import com.inditex.pricingservice.dto.InputDTO;
 import com.inditex.pricingservice.dto.OutputDTO;
+import com.inditex.pricingservice.exception.PriceNotFoundException;
 import com.inditex.pricingservice.model.Price;
 import com.inditex.pricingservice.repository.PriceRepository;
 import com.inditex.pricingservice.service.PriceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class PriceServiceImpl implements PriceService {
@@ -19,12 +17,10 @@ public class PriceServiceImpl implements PriceService {
     private PriceRepository priceRepository;
 
     public ResponseEntity<OutputDTO> findPrice(InputDTO inputDTO) {
-        Optional<Price> optionalPrice = priceRepository.findTopByBrandIdAndProductIdAndDate(
-                inputDTO.getBrandId(), inputDTO.getProductId(), inputDTO.getApplicationDate());
-
-        return optionalPrice.map(this::mapToOutputDTO)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).body(null));
+        return priceRepository.findTopByBrandIdAndProductIdAndDate(
+                        inputDTO.getBrandId(), inputDTO.getProductId(), inputDTO.getApplicationDate())
+                .map(price -> ResponseEntity.ok(mapToOutputDTO(price)))
+                .orElseThrow(() -> new PriceNotFoundException("No prices were found for the given criteria"));
     }
 
     private OutputDTO mapToOutputDTO(Price price) {
